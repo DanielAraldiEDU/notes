f9b8eb4f968d4e6b4d5d5cca27feed25f0d26d3d
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-11-25 20:12:27 -0300

    feat: created scope to the `AddNoteController` class

---
+++ src/presentation/controllers/add-note-controller.js
@@ -0,0 +1,13 @@
+import { Controller } from '../protocols';
+
+/**
+ * @public
+ * @class AddNoteController - Controller for adding notes.
+ *
+ * @extends {Controller} - It's the base class for all controllers.
+ */
+export class AddNoteController extends Controller {
+  constructor() {}
+
+  async handle(request) {}
+}
