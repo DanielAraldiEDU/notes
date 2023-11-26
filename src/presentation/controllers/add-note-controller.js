a5654991aada50b2745b8b3723d1134fd0d83fee
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-11-26 15:50:15 -0300

    feat: finished `AddNoteController` return

--- src/presentation/controllers/add-note-controller.js
+++ src/presentation/controllers/add-note-controller.js
@@ -4,14 +4,16 @@ import { Controller } from '../protocols/index.js';
 /**
  * @public
  * @class AddNoteController - Controller for adding notes.
- * @constructor validation is an instance of `Validation` class.
+ * @constructor `validation` is an instance of `Validation` class
+ * and `dbAddNote` to add the note in database, it's an `DbAddNote` class.
  *
  * @extends {Controller} - It's the base class for all controllers.
  */
 export class AddNoteController extends Controller {
-  constructor(validation) {
+  constructor(validation, dbAddNote) {
     super();
     this.validation = validation;
+    this.dbAddNote = dbAddNote;
   }
 
   async handle(request) {
@@ -19,7 +21,8 @@ export class AddNoteController extends Controller {
       const error = this.validation.validate(request);
       if (error) return badRequest(error);
 
-      return ok(null);
+      const id = await this.dbAddNote.add(request);
+      return ok({ id });
     } catch (error) {
       return serverError(error);
     }
