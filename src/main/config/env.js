c8d8cb085b86f4939c530ea7887badbda8b6efab
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-11-27 18:05:51 -0300

    feat: called `TypeOrmHelper` methods for initialize the database

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,6 +1,8 @@
 /**
  * @field port - The port to connect to on server.
+ * @field databasePort - The port to connect in database.
  */
 export default {
   port: process.env.PORT || 3333,
+  databasePort: process.env.DATABASE_PORT || 8080,
 };
--- src/main/server.js
+++ src/main/server.js
@@ -1,8 +1,15 @@
+import 'reflect-metadata';
+
 import app from './config/app.js';
 import env from './config/env.js';
 
+import { TypeOrmHelper } from './../infra/db/typeorm/index.js';
+
+TypeOrmHelper.create();
+
 try {
-  await app.listen({ port: env.port });
+  await TypeOrmHelper.initialize();
+  app.listen({ port: env.port });
 } catch (error) {
   app.log.error(error);
   process.exit(1);
